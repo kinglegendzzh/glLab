44e205f9b71e92bfe1bf591099243615f055a3a1
zhouzhanghao <zhouzh13@asiainfo.com>
2024-08-03 03:24:46 +0800

    rendering stack

---
+++ test_sdl.cpp
@@ -0,0 +1,12 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    SDL_Quit();
+    printf("SDL initialized successfully.\n");
+    return 0;
+}
